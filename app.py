a4159a9eee35d75f79d65b66c1de4b8aa0760357
Toigonbai Turgunbaev <turgunbaevt@gmail.com>
2019-12-16 22:47:14 +0600

    Updatetd app

--- app.py
+++ app.py
@@ -7,5 +7,10 @@ app = Flask(__name__)
 def hello_world():
     return "Hello world"
 
+@app.route("/test/<int:search_query>")
+def search(search_query):
+    return f"<h1>{search_query} * 100 = {search_query * 100}</h1>"
+
+
 if __name__ == "__main__":
     app.run()
