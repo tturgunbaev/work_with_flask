9fd07296b69877f4068925a785b2bd3f8d910e88
Toigonbai Turgunbaev <turgunbaevt@gmail.com>
2019-12-16 23:11:09 +0600

    Updated app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config["DEBUG"] = True
 
 @app.route('/')
 @app.route('/hello')
@@ -10,6 +11,13 @@ def hello_world():
 @app.route("/test/<int:search_query>")
 def search(search_query):
     return f"<h1>{search_query} * 100 = {search_query * 100}</h1>"
+
+@app.route("/name/<name>")
+def index(name):
+    if name.lower() == "toigonbai":
+        return f"<h1>I love you {name}</h1>", 200
+    else:
+        return f"<h1>I don't love you {name}</h1>", 404
 
 
 if __name__ == "__main__":
